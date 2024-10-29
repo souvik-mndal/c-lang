b94456aefad0d16278acc8c91a4cc6067b862d30
souvik mndal <souvkmndal@gmail.com>
2024-10-29 21:57:07 +0530

    stack using array

--- 04.stack using array.c
+++ 04.stack using array.c
@@ -3,6 +3,9 @@
 int main(){
     int choice;
     while( 1 ){
+        printf("Enter 1 for push \nEnter 2 for pop\nEnter 3 for finding the peek element\nEnter 4 for display the stack\nEnter 5 to exit ");
+        printf("Enter what you want to choose : ");
+        scanf( "%d",&choice);
         
     }
 }

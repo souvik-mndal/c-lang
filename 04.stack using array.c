a1e0c9fe61efc7515a5a7032d2f9f9b1e9a29395
souvik mndal <souvkmndal@gmail.com>
2024-10-29 22:10:51 +0530

    stack using array

--- 04.stack using array.c
+++ 04.stack using array.c
@@ -7,22 +7,37 @@ void push( int x ){
         printf("OVERFLOW");
     }
     else{
-        stack[top]=x;
         top++;
+        stack[top]=x;
+        
+    }
+}
+void display(){
+    if( top==-1 ){
+        printf("STACK IS EMPTY");
+    }
+    else{
+        for( int i=top ;i>=0 ; i-- ){
+            printf("%d ",stack[i]);
+        }
     }
 }
 int main(){
-    int choice;
+    int choice,x;
     while( 1 ){
-        printf("Enter 1 for push \nEnter 2 for pop\nEnter 3 for finding the peek element\nEnter 4 for display the stack\nEnter 5 to exit ");
+        printf("Enter 1 for push \nEnter 2 for pop\nEnter 3 for finding the peek element\nEnter 4 for display the stack\nEnter 5 to exit \n");
         printf("Enter what you want to choose : ");
         scanf( "%d",&choice);
         switch( choice ){
             case 1:
-            int x;
             printf("Enter the element you want to push : ");
             scanf("%d",&x);
             push(x);
+            printf("\n\n");
+            break;
+            case 4:
+            display();
+            printf("\n\n");
             break;
         }
     }

0248da720ad367f9328dcc24ced9139b97ed50ca
souvik mndal <souvkmndal@gmail.com>
2024-10-29 21:58:57 +0530

    stack using array

--- 04.stack using array.c
+++ 04.stack using array.c
@@ -6,6 +6,13 @@ int main(){
         printf("Enter 1 for push \nEnter 2 for pop\nEnter 3 for finding the peek element\nEnter 4 for display the stack\nEnter 5 to exit ");
         printf("Enter what you want to choose : ");
         scanf( "%d",&choice);
-        
+        switch( choice ){
+            case 1:
+            int x;
+            printf("Enter the element you want to push : ");
+            scanf("%d",&x);
+            push(x);
+            break;
+        }
     }
 }

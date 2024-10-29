d279ee51c0c9e3f3e58c769bd5d70d7811ba55f5
souvik mndal <souvkmndal@gmail.com>
2024-10-29 22:23:13 +0530

    stack using array

--- 04.stack using array.c
+++ 04.stack using array.c
@@ -12,6 +12,25 @@ void push( int x ){
         
     }
 }
+void pop(){
+    int ele;
+    if( top==-1 ){
+        printf("STACK IS EMPTY");
+    }
+    else{
+        ele=stack[top];
+        top--;
+        printf("THE LAST POPPED VALUE IS : %d ",ele);
+    }
+}
+void peek(){
+    if( top==-1 ){
+        printf("STACK IS EMPTY ");
+    }
+    else{
+        printf("THE PEEK ELEMENT IS : %d",stack[top]);
+    }
+}
 void display(){
     if( top==-1 ){
         printf("STACK IS EMPTY");
@@ -30,15 +49,29 @@ int main(){
         scanf( "%d",&choice);
         switch( choice ){
             case 1:
-            printf("Enter the element you want to push : ");
-            scanf("%d",&x);
-            push(x);
-            printf("\n\n");
-            break;
+                printf("Enter the element you want to push : ");
+                scanf("%d",&x);
+                push(x);
+                printf("\n\n");
+                break;
+            case 2:
+                pop();
+                printf("\n\n");
+                break;
+            case 3:
+                peek();
+                printf("\n\n");
+                break;
             case 4:
-            display();
-            printf("\n\n");
-            break;
+                display();
+                printf("\n\n");
+                break;
+            case 5:
+                printf("EXITED SUCCESSFULLY\n\n");
+                return 0;
+                break;
+            default:
+                printf("ENTER THE CORRECT NUMBER \n\n");
         }
     }
 }

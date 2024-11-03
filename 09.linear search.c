37e785ca7f6e1d7a78ae082930ec401283347fa9
souvik mndal <souvkmndal@gmail.com>
2024-11-03 07:21:28 +0530

    linear search

--- 09.linear search.c
+++ 09.linear search.c
@@ -3,8 +3,22 @@ int main(){
     int n;
     printf("Enter the number of elements you want to take : ");
     scanf("%d",&n);
+    int arr[n];
+    for( int i=0 ; i<n ; i++ ){
+        printf("Enter the element for index : %d ",i);
+        scanf("%d",&arr[i]);
+    }
     int x;
     printf("Enter the target element you want to find : ");
     scanf("%d",&x);
-    
+    int flag = 0 ;
+    for( int i=0 ; i<n ; i++ ){
+        if( arr[i]==x ){
+            flag=1;
+            printf("The target element is present at index : %d" , i );
+        }
+    }
+    if( flag==0 ){
+        printf("The target element is not present ");
+    }
 }

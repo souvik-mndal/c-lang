7d92801e5d304e44645c7b031dca4d2bc818e8b8
souvik mndal <souvkmndal@gmail.com>
2024-11-03 17:19:53 +0530

    binary search

---
+++ 10.binary search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of elements you want to take : ");
+    scanf("%d",&n);
+    int arr[n];
+    for( int i=0 ; i<n ; i++ ){
+        printf("Enter the element for index : %d ",i);
+        scanf("%d",&arr[i]);
+    }
+    int x;
+    printf("Enter the target element you want to find : ");
+    scanf("%d",&x);
+    int s=0 , e=n-1;
+    int flag=0;
+    while( s<=e ){
+        int mid = s+(e-s)/2;
+        if( arr[mid]==x ){
+            flag=1;
+            printf("The element is present at : %d", mid);
+        }
+        else if( arr[mid]<x ){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+    }
+    if( flag==0 ){
+        printf("Target element not found ");
+    }
+}

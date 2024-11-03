0a9ad56dce7dd8f8fc96a26fbacd6c5976177ec7
souvik mndal <souvkmndal@gmail.com>
2024-11-03 07:18:17 +0530

    linear search

---
+++ 09.linear search.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of elements you want to take : ");
+    scanf("%d",&n);
+    int x;
+    printf("Enter the target element you want to find : ");
+    scanf("%d",&x);
+    
+}

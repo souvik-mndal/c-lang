1be05747be9d008cc192aea484d216a0d6a55284
souvik mndal <souvkmndal@gmail.com>
2024-11-07 18:37:31 +0530

    creating classes and objects

--- 12.creating a class.cpp
+++ 12.creating a class.cpp
@@ -7,9 +7,10 @@ class student{
     string grade;
 };
 int main(){
-    student s1;
-    s1.name = "SM";
-    s1.age = 17;
-    s1.roll = 173;
-    s1.grade = "B+";
+    student s1 = {"SM" , 17 , 173 , "B+"};
+    cout<<s1.name<<" "<<s1.age<<" "<<s1.roll<<" "<<s1.grade;
+    // s1.name = "SM";
+    // s1.age = 17;
+    // s1.roll = 173;
+    // s1.grade = "B+";
 }

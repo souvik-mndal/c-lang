4126fae4d06762650c5578b158d61cf3b3b15fbc
souvik mndal <souvkmndal@gmail.com>
2024-11-07 18:35:06 +0530

    creating classes and objects

--- 12.creating a class.cpp
+++ 12.creating a class.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 using namespace std;
 class student{
+    public:
     string name;
     int age,roll;
     string grade;
 };
 int main(){
-    
+    student s1;
+    s1.name = "SM";
+    s1.age = 17;
+    s1.roll = 173;
+    s1.grade = "B+";
 }

e06c3212fd66106380b8be0155d0d5a30244a805
souvik mndal <souvkmndal@gmail.com>
2024-11-07 22:17:42 +0530

    dynamic memory allocation in class

---
+++ 14.dynamic memory allocation in classes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class student{
+    public:
+    string name;
+    int age,roll;
+    string grade;
+    
+};
+int main(){
+    student *s = new student;
+    (*s).name = "SM";
+    (*s).age = 17;
+    (*s).roll = 173;
+    (*s).grade = "B+";
+    
+}

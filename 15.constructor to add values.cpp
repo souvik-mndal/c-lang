d210c56a7c36f38526e922ac25e6642eb1e95b40
souvik mndal <souvkmndal@gmail.com>
2024-11-08 19:28:56 +0530

    constructor to add data

--- 15.constructor to add values.cpp
+++ 15.constructor to add values.cpp
@@ -12,7 +12,11 @@ class student{
         roll = b ;
         grade = t;
     }
+    void display( ){
+        cout<<name<<" "<<age<<" "<<roll<<" "<<grade<<endl;
+    }
 };
 int main(){
     student s1( "SM" , 17 , 173 , "B+" );
+    s1.display();
 }

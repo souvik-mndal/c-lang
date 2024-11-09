d6b37bec7d35df59ed481b3a4b506f6ba891620f
souvik mndal <souvkmndal@gmail.com>
2024-11-09 22:07:32 +0530

    static data and fn member

--- 18.static data and fn member.cpp
+++ 18.static data and fn member.cpp
@@ -19,4 +19,8 @@ int student::customer_no=0;
 int main(){
     student s1("sm" , 1000 );
     s1.display();
+    student s1("km" , 2000 );
+    s1.display();
+    student s1("bm" , 3000 );
+    s1.display();
 }

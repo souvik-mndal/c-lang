8d60bb5ee41ea93706acaaf115e3e1792a187c4f
souvik mndal <souvkmndal@gmail.com>
2024-11-10 01:06:21 +0530

    atm system

--- 20.atm system.cpp
+++ 20.atm system.cpp
@@ -14,10 +14,42 @@ class atm{
         cin>>n;
         balance = balance - n;
         if( balance < 0 ){
-            cout<<"Insufficient balance ";
+            cout<<"Insufficient balance\n ";
         }
         else{
-            cout<<"Withdrawn successfully ";
+            cout<<"Withdrawn successfully\n ";
+        }
+    }
+    void transfer(){
+        int n;
+        cout<<"Enter 1 to transfer to yourself\nEnter 2 to transfer to other\n ";
+        cout<<"Enter your choice : ";
+        cin>>n;
+        switch( n ){
+            case 1 :
+                balance +=n ;
+                cout<<"SUCCESSFULL...\n";
+                break;
+            case 2:
+                long long int acc;
+                cout<<"Enter the account number where you want to transfer : ";
+                cin>>acc;
+                balance-=n;
+                cout<<"TRANSFERED SUCCESSFULLY...\n";
+                break;
+            default:
+                cout<<"WRONG KEY...\n";
+        }
+    }
+    void viewbal(){
+        cout<<"Your current balance is : "<<balance<<endl;
+    }
+    void key(){
+        int check;
+        cout<<"Enter~"
+        if( check==pin ){
+            int n;
+            cout<<"Enter "
         }
     }
 }

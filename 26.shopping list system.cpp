437dd5b44324faba4c8f6ba831302ceeb24e1afb
souvik mndal <souvkmndal@gmail.com>
2024-11-11 00:49:51 +0530

    shopping list system

--- 26.shopping list system.cpp
+++ 26.shopping list system.cpp
@@ -4,8 +4,19 @@ using namespace std;
 // get_item display_sum remove display end
 class shopping{
     string item_code;
-    int item_code;
+    int item_price;
     static int count;
+    public:
+    void put_items(){
+        string code;
+        int price;
+        cout<<"Enter the item code : ";
+        cin>>code;
+        cout<<"Enter the price of the item : ";
+        cin>>price;
+        item_code = code;
+        item_price = price;
+    }
 };
 int shopping::count = 0;
 int main(){

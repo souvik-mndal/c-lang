a814a45d23f4824a63e6e1270bbdd915b937a1da
souvik mndal <souvkmndal@gmail.com>
2024-11-10 17:57:01 +0530

    parent and child class

---
+++ 21.parent and child class.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class parent{
+    protected:
+    int age,weight;
+};
+class child : private parent{
+    int class_no;
+    public:
+    child()
+};
+int main(){
+
+}

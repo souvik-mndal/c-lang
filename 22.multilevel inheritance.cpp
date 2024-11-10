7c36b2c4f753ac9f66b67a2da250367ff8af1802
souvik mndal <souvkmndal@gmail.com>
2024-11-10 19:49:18 +0530

    multilevel inheritance

--- 22.multilevel inheritance.cpp
+++ 22.multilevel inheritance.cpp
@@ -1,22 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-class parent{
+class grandparent{
     protected:
     int age,weight;
+};
+class parent : protected grandparent{
+    protected:
+    string gender;
 };
 class child : private parent{
     int class_no;
     public:
-    child(int age , int weight ,  int class_no ){
+    child(int age , int weight , string gender , int class_no ){
         this->age = age;
         this->weight = weight;
+        this->gender = gender;
         this->class_no = class_no;
     }
     void display(){
-        cout<<age<<" "<<weight<<" "<<class_no<<endl;
+        cout<<age<<" "<<weight<<" "<<gender<<" "<<class_no<<endl;
     }
 };
 int main(){
-    child c1( 17 , 55 , 12 );
+    child c1( 17 , 55 , "male" , 12 );
     c1.display();
 }

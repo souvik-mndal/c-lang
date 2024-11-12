1bc175267231ab885a6bad8864fb458def3dc6aa
souvik mndal <souvkmndal@gmail.com>
2024-11-12 22:31:59 +0530

    member function inside and outside

---
+++ 27.member fn inside and outside.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class value{
+    int x;
+    public:
+    void setdata( int n );
+    void getdata(){
+        cout<<"The value is : "<<x<<endl;
+    }
+};
+int main(){
+
+}

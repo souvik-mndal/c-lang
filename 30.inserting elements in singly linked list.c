16d2fb6f000840ba8250ff522ef27da193f39180
souvik mndal <souvkmndal@gmail.com>
2024-11-16 21:24:27 +0530

    inserting element in singly linkedlist

--- 30.inserting elements in singly linked list.c
+++ 30.inserting elements in singly linked list.c
@@ -5,10 +5,10 @@ int main(){
         int data;
         struct node *node;
     };
-    struct node *head=0 , *temp;
-    int choice,data;
+    struct node *head=0 , *temp , *prev=0;
+    int choice,data,i,pos;
     while(1){
-        printf("Enter 1 to create a node\nEnter 2 to insert at the start\nEnter 3 to insert at the end\nEnter 4 to insert at a position\nEner 5 to display\n");
+        printf("Enter 1 to create a node\nEnter 2 to insert at the start\nEnter 3 to insert at the end\nEnter 4 to insert at a position\nEnter 5 to display\n");
         printf("Enter the choice : ");
         scanf("%d",&choice);
         switch( choice ){
@@ -33,6 +33,36 @@ int main(){
                 firstnode->data = data;
                 firstnode->node = head;
                 head = firstnode;
+                break;
+            case 3:
+                
+                printf("Enter the element : ");
+                scanf("%d",&data);
+                struct node *lastnode = (struct node*)malloc(sizeof(struct node));
+                temp=head;
+                while( temp->node!=0 ){
+                    temp = temp->node;
+                }
+                lastnode->data = data;
+                lastnode->node = 0;
+                temp->node = lastnode;
+                break;
+            case 4:
+                i=1;
+                printf("Enter the element : ");
+                scanf("%d",&data);
+                printf("Enter the position : ");
+                scanf("%d",&pos);
+                temp = head;
+                while( i<pos){
+                    prev = temp;
+                    temp=temp->node;
+                    i++;
+                }
+                struct node *midnode = (struct node*)malloc(sizeof(struct node));
+                midnode->data = data;
+                midnode->node=temp;
+                prev->node = midnode;
                 break;
             case 5:
                 temp = head;

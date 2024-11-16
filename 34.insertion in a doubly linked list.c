1811a8a8e069650797ec1697e52e6864d0ffebb3
souvik mndal <souvkmndal@gmail.com>
2024-11-17 00:44:43 +0530

    inserting in a  doubly linkedlist

--- 34.insertion in a doubly linked list.c
+++ 34.insertion in a doubly linked list.c
@@ -6,8 +6,8 @@ int main(){
         int data;
         struct node *next;
     };
-    struct node *head=0 , *tail ,*temp;
-    int choice,data;
+    struct node *head=0 , *tail ,*temp ,*prev=0;
+    int choice,data,i,pos;
     while(1){
         printf("Enter 1 to create a node\nEnter 2 to print normally\nEnter 3 to print reversely\nEnter 4 to insert at the beginning\nEnter 5 to insert at the end\nEnter 6 to enter at a position\n");
         printf("Enter the choice : ");
@@ -51,16 +51,55 @@ int main(){
                 struct node *firstnode = (struct node*)malloc(sizeof(struct node));
                 firstnode->data = data;
                 firstnode->previous=0;
-                firstnode->next=head;
-                head->previous=firstnode;
-                head=firstnode;
+                firstnode->next=0;
+                if(head==0 ){
+                    head=firstnode;
+                    tail=firstnode;
+                }
+                else{
+                    firstnode->next=head;
+                    head->previous=firstnode;
+                    head=firstnode;
+                }
+                
                 break;
-            // case 5:
+            case 5:
+                printf("Enter the element : ");
+                scanf("%d",&data);
+                struct node *endnode = (struct node*)malloc(sizeof(struct node));
+                endnode->data=data;
+                endnode->next=0;
+                endnode->previous=0;
+                if(tail==0){
+                    head=endnode;
+                    tail=endnode;
+                }
+                else{
+                    endnode->previous=tail;
+                    tail->next=endnode;
+                    tail=endnode; 
+                }
                 
-            //     break;
-            // case 6:
-                
-            //     break;
+                break;
+            case 6:
+                printf("Enter the element : ");
+                scanf("%d",&data);
+                printf("Enter the position : ");
+                scanf("%d",&pos);
+                struct node *middlenode = (struct node*)malloc(sizeof(struct node));
+                middlenode->data=data;
+                i=1;
+                temp=head;
+                while( i<pos ){
+                    prev=temp;
+                    temp=temp->next;
+                    i++;
+                }
+                middlenode->next=temp;
+                temp->previous=middlenode;
+                middlenode->previous=prev;
+                prev->next=middlenode;
+                break;
         }
     }
     
